7c640b92eefa11e029608baea2296758c70fe98b
jawadboughalem <jawadboughalem91@gmail.com>
2023-09-04 16:59:13 +0200

    Rectif Footer

--- src/pages/notFound/NotFound.js
+++ src/pages/notFound/NotFound.js
@@ -1,18 +1,21 @@
-import './notFound.scss'
+import './notFound.scss';
+import React from 'react';
 import Header from '../../components/header/Header';
 import { Link } from 'react-router-dom';
 import Footer from '../../components/footer/Footer';
 
 export default function NotFound() {
-  return(
-    <div className='notFound'>
-        <Header />
+  return (
+    <div>
+      <Header />
+      <div className='notFound'>
         <div className="notFound_infos">
           <h1 className='notFound_infos_title'>404</h1>
           <p className='notFound_infos_content'>La page que vous recherchez n'existe pas.</p>
         </div>
         <Link className='notFound_infos_return' to='/'>Retourner sur la page d'acceuil</Link>
-        <Footer />
+      </div>
+      <Footer />
     </div>
-  )
+  );
 }
